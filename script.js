c2aaf5710467b204dc53de83ae9b2accffae366a
Seb887 <seb887.dev@gmail.com>
2023-01-12 22:50:19 +0100

    second commit

--- script.js
+++ script.js
@@ -3,28 +3,44 @@
 // Important variables
 const gridContainer = document.querySelector('.grid-container');
 
+const slider = document.querySelector('#myRange');
+const sliderDisplay = document.querySelector('#sliderDisplay');
+
+const btnColor = document.querySelector('#btn-color');
+const btnRainbow = document.querySelector('#btn-rainbow');
+const btnClear = document.querySelector('#btn-clear');
+
 // Defaults
-const defaultGrid = 64;
+const defaultGridSize = 16;
 const defaultColor = 'black';
 
 // Setup Grid
+
 function setupGrid(gridSize) {
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     let gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element');
+    gridElement.classList.add('grid-element', 'border');
     gridContainer.append(gridElement);
   }
+
+  updateSliderDisplay();
 }
 
-setupGrid(defaultGrid);
+setupGrid(slider.value);
 
-// Get Buttons
-const btnColor = document.querySelector('#btn-color');
-const btnRainbow = document.querySelector('#btn-rainbow');
-const btnClear = document.querySelector('#btn-clear');
+function updateSliderDisplay() {
+  sliderDisplay.textContent = slider.value;
+}
+
+slider.addEventListener('mousemove', () => {
+  sliderDisplay.textContent = slider.value;
+  updateSliderDisplay();
+  removeGrid();
+  setupGrid(slider.value);
+});
 
 // Color Management
 let changeColor = defaultColor;
@@ -33,10 +49,11 @@ let setColor;
 // Set Color
 btnColor.addEventListener('click', () => {
   setColor = prompt('Set Color:');
+  console.log('New color =', setColor);
   changeColor = setColor;
 });
 
-// Set Rainbow
+// Create Rainbow
 function createRainbowColor() {
   let colorR = Math.floor(Math.random() * 256);
   let colorG = Math.floor(Math.random() * 256);
@@ -51,21 +68,29 @@ btnRainbow.addEventListener('click', () => {
 // Mouse event
 const gridElements = document.querySelectorAll('.grid-element');
 
+function changeColorGrid(element) {
+  if (changeColor === setColor || changeColor === 'black') {
+    element.style.backgroundColor = changeColor;
+  } else {
+    createRainbowColor();
+    element.style.backgroundColor = changeColor;
+  }
+}
+
 gridElements.forEach((element) => {
-  element.addEventListener('mouseover', () => {
-    if (changeColor === setColor || changeColor === 'black') {
-      element.style.backgroundColor = changeColor;
-    } else {
-      createRainbowColor();
-      element.style.backgroundColor = changeColor;
-    }
-  });
+  element.addEventListener('mouseover', (e) => changeColorGrid(e.target));
 });
 
 // Clear grid color
-btnClear.addEventListener('click', () => {
-  gridElements.forEach((element) => (element.style.backgroundColor = 'white'));
-  changeColor = defaultColor;
-});
+function removeGrid() {
+  gridContainer.innerHTML = '';
+}
+
+function clearGrid() {
+  gridElements.forEach((element) => element.removeAttribute('style'));
+  // changeColor = defaultColor;
+}
+
+btnClear.addEventListener('click', clearGrid);
 
 // Slider grid size
