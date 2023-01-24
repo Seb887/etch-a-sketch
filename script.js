9fe118e8bb926c1e301f2e2fe546a43e452e9617
Seb887 <seb887.dev@gmail.com>
2023-01-24 13:40:17 +0100

    create Grid

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
-const grid = document.querySelector('.grid-container');
+const gridContainer = document.querySelector('.grid-container');
+
+function setupGrid(gridSize) {
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let gridElement = document.createElement('div');
+    gridElement.classList.add('grid-element', 'border');
+    gridContainer.appendChild(gridElement);
+  }
+}
+
+setupGrid(8);
